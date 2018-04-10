45c5b37a710058f19027ac7b612fbcf248576abe
rqxiao <1007689622@qq.com>
2018-04-10 16:54:42 +0800

    0410

---
+++ src/test/java/com/example/demo/dao/AreaDaoTest.java
@@ -0,0 +1,28 @@
+package com.example.demo.dao;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AreaDaoTest {
+
+    @Test
+    public void queryArea() {
+    }
+
+    @Test
+    public void queryAreaById() {
+    }
+
+    @Test
+    public void insertArea() {
+    }
+
+    @Test
+    public void updateArea() {
+    }
+
+    @Test
+    public void deleteArea() {
+    }
+}
