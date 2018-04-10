6911a700a7c888f5239151c6aa2870e5f8fc3078
rqxiao <1007689622@qq.com>
2018-04-10 17:32:15 +0800

    0410

--- src/test/java/com/example/demo/dao/AreaDaoTest.java
+++ src/test/java/com/example/demo/dao/AreaDaoTest.java
@@ -1,10 +1,18 @@
 package com.example.demo.dao;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.*;
-
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class AreaDaoTest {
+
+    @Autowired
+    private AreaDao areaDao;
 
     @Test
     public void queryArea() {
